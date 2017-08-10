f4bad58969f7088c83a1384fcf9b89709b8ebbf1
Pardis Ranjbar <pardis.ranjbar@gmail.com>
2017-08-10 16:42:57 -0400

    Create last_character_of_word.py

---
+++ last_character_of_word.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on August 10th, 2017
+
+@author: Pardis Ranjbar
+E-mail: pardis.ranjbar@gmail.com
+#==============================================================================
+# This code gets a string from user and prints the last character in the string.
+#==============================================================================
+"""
+word=input('enter the word:\n')
+word_size=len(word)
+print(word[word_size-1])
